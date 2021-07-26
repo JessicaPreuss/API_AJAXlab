27275ef84636404c5e175a056fb8604af4fbcee7
JessicaPreuss <82731575+JessicaPreuss@users.noreply.github.com>
2021-07-26 14:06:43 -0400

    css

--- script.js
+++ script.js
@@ -5,8 +5,22 @@ fetch("https://www.reddit.com/r/aww/.json")
 .then(data => {
     //create variable to hold the parent info we don't need
     let something = data.data.children; //now this is an array
-    for (let i=0; i<something.length; i++) {
-        console.log(something[i].data.title)
+    for (let i=0; i<10; i++) {
+        const newDiv = document.createElement("div");
+        document.querySelector(".posts").appendChild(newDiv);
+        const title = document.createElement("p");
+        title.innerText = something[i].data.title;
+        newDiv.appendChild(title);
+        const image = document.createElement("img");
+        image.src = something[i].data.thumbnail;
+        newDiv.appendChild(image);
+        const link = document.createElement("a");
+        link.href = something[i].data.url;
+        link.innerText = "Click HERE for the orignal post";
+        newDiv.appendChild(link);
+
     }
  
  })
+
+ 
